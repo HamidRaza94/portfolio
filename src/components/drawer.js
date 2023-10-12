e8bbf55b3012677f2c073f7712146fc1d70f3f95
HamidRaza94 <raza.hamid09@gmail.com>
2023-10-13 02:10:50 +0530

    initial commit

--- src/components/drawer.js
+++ src/components/drawer.js
@@ -7,12 +7,7 @@ const Drawer = (props) => {
     <div
       className={classNames(
         'fixed left-0 right-0 z-20 h-36 w-full transition-all duration-500 transform bg-white dark:bg-dark-grey dark:text-white',
-        {
-          'translate-y-0': open,
-          'bottom-12': open,
-          'translate-y-full': !open,
-          'bottom-0': !open,
-        },
+        { 'translate-y-0': open, 'bottom-12': open, 'translate-y-full': !open, 'bottom-0': !open },
       )}
     >
       <div className="p-4">
--- src/pages/index.js
+++ src/pages/index.js
@@ -46,7 +46,7 @@ export default function HomeApp() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-gray-100 dark:bg-darkest-grey z-0">
+      <main className="bg-gray-100 dark:bg-darkest-grey z-0 transition-all duration-500 transform">
         <section ref={home}>
           <HomePage />
         </section>
