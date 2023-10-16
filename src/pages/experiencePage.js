1f5e0701a06a94738e1d88a70e8a5ecf22f9b089
HamidRaza94 <raza.hamid09@gmail.com>
2023-10-17 00:54:16 +0530

    initial commit

--- src/pages/experiencePage.js
+++ src/pages/experiencePage.js
@@ -12,8 +12,15 @@ import ThemeModeContext from '@/contexts/ThemeModeContext';
 
 import IntersoftCompanyLightImage from '@/assets/images/companies/intersoft-light.png';
 import IntersoftCompanyDarkImage from '@/assets/images/companies/intersoft-dark.png';
-import ReactJSImage from '@/assets/images/social-media/reactjs.png';
-import ReduxJSImage from '@/assets/images/social-media/reduxjs.png';
+import HTCCompanyImage from '@/assets/images/companies/htc.png';
+import SuccessiveCompanyImage from '@/assets/images/companies/successive-technologies.png';
+import ReactJSImage from '@/assets/images/technologies/reactjs.png';
+import ReduxJSImage from '@/assets/images/technologies/reduxjs.png';
+import AngularImage from '@/assets/images/technologies/angular.png';
+import GraphQLImage from '@/assets/images/technologies/graphql.png';
+import NodeJSImage from '@/assets/images/technologies/nodejs.png';
+import ExpressJSImage from '@/assets/images/technologies/expressjs.png';
+import MongoDBImage from '@/assets/images/technologies/mongodb.png';
 import ProfileIcon from '@/assets/icons/profile.svg';
 import CalenderIcon from '@/assets/icons/calender.svg';
 import ProjectsIcon from '@/assets/icons/projects-16*16.svg';
@@ -28,15 +35,51 @@ const Experience = (_, ref) => {
       <PageHeading label="Experience" ref={ref} />
 
       <div className="flex">
-        <Image
-          src={isLightTheme ? IntersoftCompanyLightImage : IntersoftCompanyDarkImage}
-          alt="Intersoft Company Logo"
-          className="w-24 h-10"
-        />
+        <div className="flex flex-col items-end">
+          <Image
+            src={HTCCompanyImage}
+            alt="Intersoft Company Logo"
+            className="w-24 h-8"
+          />
+
+          <Spacer y={32} />
+
+          <div className="flex flex-col items-end mt-10">
+            <div className="flex items-center">
+              <Label text="technical analyst" size="text-xl" isBold />
+              <Spacer x={1} />
+              <ProfileIcon color={isLightTheme ? '' : 'white'} />
+            </div>
+            <div className="flex items-center">
+              <Label text="sept 2021 - mar 2023" isItalic isThin style="leading-5" />
+              <Spacer x={1} />
+              <CalenderIcon fill={isLightTheme ? '' : 'white'} />
+            </div>
+            <div className="flex items-center">
+              <Label text="done 1 project" isThin style="leading-5" />
+              <ProjectsIcon color={isLightTheme ? '' : 'white'} />
+            </div>
+            <Spacer y={2} />
+
+            <div className="flex justify-start">
+              <Logo img={ReactJSImage} alt="React JS Logo" />
+              <Spacer x={2} />
+              <Logo img={ReduxJSImage} alt="Redux JS Logo" />
+            </div>
+          </div>
+
+          <Spacer y={5} />
+
+          <Image
+            src={SuccessiveCompanyImage}
+            alt="Successive Technologies Company Logo"
+            className="w-24 h-12"
+          />
+        </div>
 
         <Spacer x={3} />
 
-        <div className="flex flex-col items-center mt-3">
+        <div className="flex flex-col items-center mt-2">
           <JourneyBar />
           <JourneyBar />
           <JourneyBar />
@@ -44,29 +87,75 @@ const Experience = (_, ref) => {
 
         <Spacer x={3} />
 
-        <div className="flex flex-col items-start mt-10">
-          <div className="flex items-center">
-            <ProfileIcon color={isLightTheme ? '' : 'white'} />
-            <Spacer x={1} />
-            <Label text="technical analyst" size="text-xl" isBold />
-          </div>
-          <div className="flex items-center">
-            <CalenderIcon fill={isLightTheme ? '' : 'white'} />
-            <Spacer x={1} />
-            <Label text="sept 2021 - mar 2023" isItalic isThin style="leading-5" />
-          </div>
-          <div className="flex items-center">
-            <ProjectsIcon color={isLightTheme ? '' : 'white'} />
-            <Label text="done 1 project" isThin style="leading-5" />
-          </div>
-          <Spacer y={2} />
+        <div className="flex flex-col items-start mt-8">
+          <div>
+            <div className="flex items-center">
+              <ProfileIcon color={isLightTheme ? '' : 'white'} />
+              <Spacer x={1} />
+              <Label text="senior engineer" size="text-xl" isBold />
+            </div>
+            <div className="flex items-center">
+              <CalenderIcon fill={isLightTheme ? '' : 'white'} />
+              <Spacer x={1} />
+              <Label text="mar 2023 - present" isItalic isThin style="leading-5" />
+            </div>
+            <div className="flex items-center">
+              <ProjectsIcon color={isLightTheme ? '' : 'white'} />
+              <Label text="done 2 projects" isThin style="leading-5" />
+            </div>
+            <Spacer y={2} />
 
-          <div className="flex justify-start">
-            <Logo img={ReactJSImage} alt="React JS Logo" />
-            <Spacer x={2} />
-            <Logo img={ReduxJSImage} alt="Redux JS Logo" />
+            <div className="flex justify-start">
+              <Logo img={ReactJSImage} alt="React JS Logo" />
+              <Spacer x={2} />
+              <Logo img={ReduxJSImage} alt="Redux JS Logo" />
+              <Spacer x={2} />
+              <Logo img={AngularImage} alt="Angular JS" />
+            </div>
+          </div>
+
+          <Spacer y={8} />
+
+          <Image
+            src={isLightTheme ? IntersoftCompanyLightImage : IntersoftCompanyDarkImage}
+            alt="Intersoft Company Logo"
+            className="w-24 h-10"
+          />
+
+          <Spacer y={36} />
+
+          <div>
+            <div className="flex items-center">
+              <ProfileIcon color={isLightTheme ? '' : 'white'} />
+              <Spacer x={1} />
+              <Label text="associate engineer" size="text-xl" isBold />
+            </div>
+            <div className="flex items-center">
+              <CalenderIcon fill={isLightTheme ? '' : 'white'} />
+              <Spacer x={1} />
+              <Label text="jan 2019 - sept 2021" isItalic isThin style="leading-5" />
+            </div>
+            <div className="flex items-center">
+              <ProjectsIcon color={isLightTheme ? '' : 'white'} />
+              <Label text="done 4 projects" isThin style="leading-5" />
+            </div>
+
+            <Spacer y={2} />
+
+            <div className="flex flex-wrap">
+              <Logo img={ReactJSImage} alt="React JS Logo" />
+              <Spacer x={2} />
+              <Logo img={GraphQLImage} alt="GraphQL JS" />
+              <Spacer x={2} />
+              <Logo img={NodeJSImage} alt="Node JS" />
+              <Spacer x={2} />
+              <Logo img={ExpressJSImage} alt="Express JS" />
+              <Spacer x={2} />
+              <Logo img={MongoDBImage} alt="Mongo DB" />
+            </div>
           </div>
         </div>
+
       </div>
     </div>
   );
