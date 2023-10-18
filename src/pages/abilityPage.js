ebe247a1a4091229936c1fae1e7eb064aa5f2ca4
HamidRaza94 <raza.hamid09@gmail.com>
2023-10-18 17:19:54 +0530

    initial commit

--- src/pages/abilityPage.js
+++ src/pages/abilityPage.js
@@ -1,10 +1,29 @@
 import { forwardRef } from 'react';
 import classNames from 'classnames';
 
+import PageHeading from '@/components/pageHeading';
+
+import FrontEndIcon from '@/assets/icons/front-end.svg';
+import BackEndIcon from '@/assets/icons/back-end.svg';
+import Label from '@/components/label';
+import Spacer from '@/components/spacer';
+
 const Ability = (_, ref) => {
   return (
-    <div className={classNames('')}>
-      Ability Page
+    <div className={classNames('flex flex-col items-center justify-center')}>
+      <PageHeading label="Ability" ref={ref} />
+
+        <div className="flex flex-col justify-center items-center">
+          <FrontEndIcon width={150} />
+          <Label text="Front-end designing" />
+        </div>
+
+        <Spacer size={150} axis="vertical" />
+
+        <div className="flex flex-col justify-center items-center">
+          <BackEndIcon width={150} />
+          <Label text="Back-end designing" />
+        </div>
     </div>
   );
 }
