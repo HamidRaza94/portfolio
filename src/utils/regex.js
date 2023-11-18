dcae2e551debfb1c44001f0b21b363f9d12ea980
Mohammad Hamid Raza <raza.hamid09@gmail.com>
2023-11-18 21:32:43 +0530

    initial commit

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -1,3 +1,3 @@
-export const NAME_REGEX = /(^[A-Za-z]{3,16})([ ]{0,1})([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})/;
+export const NAME_REGEX = /^[A-Za-z\s]{1,}[\.]{0,1}[A-Za-z\s]{0,}$/;
 
 export const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
